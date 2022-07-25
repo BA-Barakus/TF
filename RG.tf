9987fad1dae27412e049aa517a23813ea58bc938
BA-Barakus <109948946+BA-Barakus@users.noreply.github.com>
2022-07-25 10:09:42 +0200

    Create RG.tf

---
+++ RG.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.13.0"
+    }
+  }
+  }
+provider "azurerm" {
+  # Configuration options
+  features {}
+  subscription_id = "PUT-YOUR_SUB-ID"
+}
+
+
+resource "azurerm_resource_group" "RG" {
+count =10 #This value will create 10 RG
+name = "Student-${count.index}"
+location = "West Europe"
+}
